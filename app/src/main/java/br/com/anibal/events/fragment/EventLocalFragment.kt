904e869e40875dd6c9eb81b0bd6123435c8ece0e
User <vinivieiradossantos@gmail.com>
2018-06-11 20:28:27 -0300

    EventDetailActivity - Local
    Contém bugs.

--- app/src/main/java/br/com/anibal/events/fragment/EventLocalFragment.kt
+++ app/src/main/java/br/com/anibal/events/fragment/EventLocalFragment.kt
@@ -7,12 +7,37 @@ import android.view.View
 import android.view.ViewGroup
 
 import br.com.anibal.events.R
+import br.com.anibal.events.domain.Arguments
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class EventLocalFragment : BaseFragment() {
+class EventLocalFragment : BaseFragment(), OnMapReadyCallback {
+
+    private val event = Arguments.event
+    private var googleMap: GoogleMap? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_event_local, container, false)
+        val view = inflater.inflate(R.layout.fragment_event_local, container, false)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        return view
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        this.googleMap = googleMap
+        val location = LatLng(event.latitude.toDouble(), event.longitude.toDouble())
+        val latLngZoom = CameraUpdateFactory.newLatLngZoom(location, 17f)
+        this.googleMap?.moveCamera(latLngZoom)
+        this.googleMap?.addMarker(MarkerOptions()
+                .title(event.name)
+                .snippet(event.startDate + " a " + event.finishDate)
+                .position(location))
+        this.googleMap?.mapType = GoogleMap.MAP_TYPE_NORMAL
     }
 
 
